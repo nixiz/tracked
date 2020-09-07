408612bfacb04cec70f4e19f33a3d6115baaf16c
Oguzhan KATLI <oguzhankatli@gmail.com>
2020-09-07 09:35:47 +0300

    -.

--- out/build/x64-Debug (default)/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/SW/tracked/test
-# Build directory: C:/SW/tracked/out/build/x64-Debug (default)/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(trackedTests "C:/SW/tracked/out/build/x64-Debug (default)/test/trackedTests.exe")
-set_tests_properties(trackedTests PROPERTIES  _BACKTRACE_TRIPLES "C:/SW/tracked/test/CMakeLists.txt;81;add_test;C:/SW/tracked/test/CMakeLists.txt;0;")
